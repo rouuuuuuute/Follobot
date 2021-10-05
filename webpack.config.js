80a736d65e2c0dc7248bfe2d16170d35eaeb2f6b
タカヨシ <>
2021-10-05 16:05:46 +0900

    フロント環境構築

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'resources/js/app.js'),
+    output: {
+        path: path.join(__dirname, 'public/js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        modules: [path.join(__dirname, 'src'),'node_modules'],
+        extensions: ['.js','.jsx'],
+        alias:{
+            vue: 'vue/dist/vue.esm.js'
+        }
+    }
+}
